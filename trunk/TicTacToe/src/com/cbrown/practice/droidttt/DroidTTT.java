fc59f3989560e6f967231d1eb034c279ae2a2daa
cmanbrown <cmanbrown@gmail.com>
2011-05-05 00:54:28 +0000

    Adding Droid TTT

---
+++ trunk/TicTacToe/src/com/cbrown/practice/droidttt/DroidTTT.java
@@ -0,0 +1,128 @@
+package com.cbrown.practice.droidttt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Toast;
+
+public class DroidTTT extends Activity {
+
+	CanvasView canvasView;
+	TicTacToeBoard ticTacToeBoard;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.my_layout);
+
+		setContentView(R.layout.my_layout);
+
+		canvasView = (CanvasView) findViewById(R.id.cavasView);
+		canvasView.setOnTouchListener(mTouchListener);
+
+		ticTacToeBoard = new TicTacToeBoard();
+
+		ticTacToeBoard.resetBoard();
+
+		ticTacToeBoard.setmBoardBitmap(canvasView.loadBitmap(this,
+				R.drawable.new_grid));
+		canvasView.setmBackgroundBitmap(ticTacToeBoard.getmBoardBitmap());
+
+		ticTacToeBoard.setmPlayerOBitmap(canvasView.loadBitmap(this,
+				R.drawable.android));
+		canvasView.setmPlayerOBitmap(ticTacToeBoard.getmPlayerOBitmap());
+
+		ticTacToeBoard.setmPlayerXBitmap(canvasView.loadBitmap(this,
+				R.drawable.android2));
+		canvasView.setmPlayerXBitmap(ticTacToeBoard.getmPlayerXBitmap());
+
+		canvasView.setmGameBoardCells(ticTacToeBoard.getmGameBoardCells());
+		canvasView.invalidate();
+
+		// DisplayMetrics dm = new DisplayMetrics();
+		// getWindowManager().getDefaultDisplay().getMetrics(dm);
+		//
+		// Toast.makeText(this,
+		// "Width: " + dm.widthPixels + "\n" +
+		// "Height: " + dm.heightPixels + "\n", Toast.LENGTH_LONG).show();
+		//
+
+	}
+
+	View.OnTouchListener mTouchListener = new OnTouchListener() {
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			float rawX = event.getRawX();
+			float rawY = event.getRawY();
+
+			// int translatedX = (int) rawX / 160 * 160;
+			// int translatedY = (int) rawY / 160 * 160;
+
+			int currentGameStatus = ticTacToeBoard
+					.getGameStatusAndCheckForWinner();
+			int gameBoardCell = (int) rawX / 160 + (int) rawY / 160 * 3;
+
+			// Toast.makeText(v.getContext(), "X: " +
+			// rawX + " Y: " + rawY, Toast.LENGTH_LONG).show();
+			// Toast.makeText(v.getContext(),
+			// "Translated X: " + translatedX +
+			// "\nTranslated Y: " + translatedY +
+			// "\nRawX: " + rawX +
+			// "\nRawY: " + rawY +
+			// "\nBameboard Contrib from X: " + (int) rawX / 160 +
+			// "\nBameboard Contrib from Y: " + (int) rawY / 160 * 3 +
+			// "\nGameboard cell: " + gameBoardCell, Toast.LENGTH_LONG).show();
+
+			// Pre-turn Check
+			// if (currentGameStatus == TicTacToeBoard.GameStatus.GAME_IN_PLAY)
+			// if (gameBoardCell < 9) {
+			// ticTacToeBoard.consumeTurn(gameBoardCell);
+			// //canvasView.invalidate();
+			// currentGameStatus =
+			// ticTacToeBoard.getGameStatusAndCheckForWinner();
+			// }
+
+			switch (currentGameStatus) {
+			case TicTacToeBoard.GameStatus.GAME_IN_PLAY:
+				if (gameBoardCell < 9) {
+					ticTacToeBoard.consumeTurn(gameBoardCell);
+					currentGameStatus = ticTacToeBoard
+							.getGameStatusAndCheckForWinner();
+				}
+					break;
+			case TicTacToeBoard.GameStatus.GAME_OVER_TIE:
+			case TicTacToeBoard.GameStatus.PLAYER_O_WINS:
+			case TicTacToeBoard.GameStatus.PLAYER_X_WINS:
+			default:
+				ticTacToeBoard.resetBoard();
+				canvasView.invalidate();
+				return false;
+			}
+
+			// If the game is over, display a message and reset the game
+			switch (currentGameStatus) {
+			case TicTacToeBoard.GameStatus.PLAYER_O_WINS:
+				Toast.makeText(v.getContext(), "Player O Wins!",
+						Toast.LENGTH_LONG).show();
+				break;
+			case TicTacToeBoard.GameStatus.PLAYER_X_WINS:
+				Toast.makeText(v.getContext(), "Player X Wins!",
+						Toast.LENGTH_LONG).show();
+				break;
+			case TicTacToeBoard.GameStatus.GAME_OVER_TIE:
+				Toast.makeText(v.getContext(), "It was a Tie!",
+						Toast.LENGTH_LONG).show();
+				break;
+			default:
+				break;
+
+			}
+
+			canvasView.invalidate();
+			return false;
+		}
+	};
+}
---
+++ trunk/TicTacToe/src/com/cbrown/practice/droidttt/TicTacToeBoard.java
@@ -0,0 +1,153 @@
+package com.cbrown.practice.droidttt;
+
+import android.graphics.Bitmap;
+
+public class TicTacToeBoard {
+	private Bitmap mBoardBitmap;
+	private Bitmap mPlayerXBitmap;
+	private Bitmap mPlayerOBitmap;
+
+	private int[] mGameBoardCells = new int[9];
+	// private int mGameStatus;
+	private int mCurrentPlayer;
+
+	public void setmBoardBitmap(Bitmap mBoardBitmap) {
+		this.mBoardBitmap = mBoardBitmap;
+	}
+
+	public Bitmap getmBoardBitmap() {
+		return mBoardBitmap;
+	}
+
+	public void setmPlayerXBitmap(Bitmap mPlayerXBitmap) {
+		this.mPlayerXBitmap = mPlayerXBitmap;
+	}
+
+	public Bitmap getmPlayerXBitmap() {
+		return mPlayerXBitmap;
+	}
+
+	public void setmPlayerOBitmap(Bitmap mPlayerOBitmap) {
+		this.mPlayerOBitmap = mPlayerOBitmap;
+	}
+
+	public Bitmap getmPlayerOBitmap() {
+		return mPlayerOBitmap;
+	}
+
+	public int getCellStatus(int cell) {
+		return getmGameBoardCells()[cell];
+	}
+
+	public void setCellStatus(int cell, int status) {
+		getmGameBoardCells()[cell] = status;
+	}
+
+	public void consumeTurn(int attemptedCell) {
+		// If a move was made, set the targeted cell to the current player, and
+		// then cycle the player
+		if (getmGameBoardCells()[attemptedCell] == CellStatus.EMPTY) {
+			getmGameBoardCells()[attemptedCell] = mCurrentPlayer;
+			if (mCurrentPlayer == CellStatus.PLAYER_O)
+				mCurrentPlayer = CellStatus.PLAYER_X;
+			else
+				mCurrentPlayer = CellStatus.PLAYER_O;
+		}
+	}
+
+	public int getGameStatusAndCheckForWinner() {
+		for (int x = 0; x < 3; x++) {
+			// Check each row for Player X
+			if (getmGameBoardCells()[(x * 3)] == TicTacToeBoard.CellStatus.PLAYER_X
+					&& getmGameBoardCells()[(x * 3 + 1)] == TicTacToeBoard.CellStatus.PLAYER_X
+					&& getmGameBoardCells()[(x * 3 + 2)] == TicTacToeBoard.CellStatus.PLAYER_X)
+				return TicTacToeBoard.GameStatus.PLAYER_X_WINS;
+
+			// Check each column X for Player X
+			if (getmGameBoardCells()[(x)] == TicTacToeBoard.CellStatus.PLAYER_X
+					&& getmGameBoardCells()[(x + 3)] == TicTacToeBoard.CellStatus.PLAYER_X
+					&& getmGameBoardCells()[(x + 6)] == TicTacToeBoard.CellStatus.PLAYER_X)
+				return TicTacToeBoard.GameStatus.PLAYER_X_WINS;
+
+			// Check Row X for Player O
+			if (getmGameBoardCells()[(x * 3)] == TicTacToeBoard.CellStatus.PLAYER_O
+					&& getmGameBoardCells()[(x * 3 + 1)] == TicTacToeBoard.CellStatus.PLAYER_O
+					&& getmGameBoardCells()[(x * 3 + 2)] == TicTacToeBoard.CellStatus.PLAYER_O)
+				return TicTacToeBoard.GameStatus.PLAYER_O_WINS;
+
+			// Check Column X for Player O
+			if (getmGameBoardCells()[(x)] == TicTacToeBoard.CellStatus.PLAYER_O
+					&& getmGameBoardCells()[(x + 3)] == TicTacToeBoard.CellStatus.PLAYER_O
+					&& getmGameBoardCells()[(x + 6)] == TicTacToeBoard.CellStatus.PLAYER_O)
+				return TicTacToeBoard.GameStatus.PLAYER_O_WINS;
+		}
+
+		// Check Diagonals for Player X
+		if (getmGameBoardCells()[(0)] == TicTacToeBoard.CellStatus.PLAYER_X
+				&& getmGameBoardCells()[(4)] == TicTacToeBoard.CellStatus.PLAYER_X
+				&& getmGameBoardCells()[(8)] == TicTacToeBoard.CellStatus.PLAYER_X)
+			return TicTacToeBoard.GameStatus.PLAYER_X_WINS;
+
+		if (getmGameBoardCells()[(6)] == TicTacToeBoard.CellStatus.PLAYER_X
+				&& getmGameBoardCells()[(4)] == TicTacToeBoard.CellStatus.PLAYER_X
+				&& getmGameBoardCells()[(2)] == TicTacToeBoard.CellStatus.PLAYER_X)
+			return TicTacToeBoard.GameStatus.PLAYER_X_WINS;
+
+		// Check Diagonals for Player O
+		if (getmGameBoardCells()[(0)] == TicTacToeBoard.CellStatus.PLAYER_O
+				&& getmGameBoardCells()[(4)] == TicTacToeBoard.CellStatus.PLAYER_O
+				&& getmGameBoardCells()[(8)] == TicTacToeBoard.CellStatus.PLAYER_O)
+			return TicTacToeBoard.GameStatus.PLAYER_O_WINS;
+
+		if (getmGameBoardCells()[(6)] == TicTacToeBoard.CellStatus.PLAYER_O
+				&& getmGameBoardCells()[(4)] == TicTacToeBoard.CellStatus.PLAYER_O
+				&& getmGameBoardCells()[(2)] == TicTacToeBoard.CellStatus.PLAYER_O)
+			return TicTacToeBoard.GameStatus.PLAYER_O_WINS;
+
+		boolean movesLeft = false;
+		for (int x = 0; x < getmGameBoardCells().length; x++) {
+			if (getmGameBoardCells()[x] == CellStatus.EMPTY)
+				movesLeft = true;
+		}
+
+		if (!movesLeft)
+			return GameStatus.GAME_OVER_TIE;
+		return GameStatus.GAME_IN_PLAY;
+	}
+
+	public void resetBoard() {
+		// Set each cell to EMPTY
+		for (int x = 0; x < getmGameBoardCells().length; x++) {
+			getmGameBoardCells()[x] = CellStatus.EMPTY;
+		}
+
+		// Set the Game Status to GAME_IN_PLAY
+		// mGameStatus = GameStatus.GAME_IN_PLAY;
+
+		// Set the first player to Player X;
+		mCurrentPlayer = CellStatus.PLAYER_X;
+	}
+
+	public void setmGameBoardCells(int[] mGameBoardCells) {
+		this.mGameBoardCells = mGameBoardCells;
+	}
+
+	public int[] getmGameBoardCells() {
+		return mGameBoardCells;
+	}
+
+	public static final class CellStatus {
+		public static final int EMPTY = 0;
+		public static final int PLAYER_X = 1;
+		public static final int PLAYER_O = 2;
+
+	}
+
+	public static final class GameStatus {
+		public static final int GAME_IN_PLAY = 0;
+		public static final int PLAYER_X_WINS = 1;
+		public static final int PLAYER_O_WINS = 2;
+		public static final int GAME_OVER_TIE = 3;
+	}
+
+}
