5bc4740a0276b7e32c3e0013e1cafa40c0775550
cmanbrown <cmanbrown@gmail.com>
2011-05-05 01:01:09 +0000

    Importing Droid TTT again, (this time the right way!)

---
+++ trunk/TicTacToe/Droid TTT/src/com/cbrown/practice/droidttt/Renderable.java
@@ -0,0 +1,12 @@
+package com.cbrown.practice.droidttt;
+
+public abstract class Renderable {
+	// Position.
+	public float x;
+	public float y;
+	public float z;
+
+	// Size.
+	public float width;
+	public float height;
+}
